c2454432f2a6b5a16ffd3a7cc47b0d7f340ea646
rnrgll <shuajjjjj@naver.com>
2025-05-30 09:36:48 +0900

    [Feat] 단서 노트 UI 생성 #5 #10

---
+++ Assets/Scripts/Content/UI/ClueNoteUI.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Content.UI
+{
+    public class ClueNoteUI : BaseUI
+    {
+        
+        // public List<> panels = new List<PanelItem>();
+        //
+        //
+        //
+        // [SerializeField] private TMP_Text _clueTitle;
+        // [SerializeField] private TMP_Text _clueDescription;
+        // [SerializeField] private TMP_Text _playerThought;
+        //
+        //
+        // public void SetClueNoteData()
+        // {
+        //     //todo: ui 업데이트
+        //     Debug.Log("버튼 클릭");
+        // }
+        //
+        //
+        // public void OpenPanel(string newPanel)
+        // {
+        //     for (int i = 0; i < panels.Count; i++)
+        //     {
+        //         if (panels[i].panelName == newPanel)
+        //             newPanelIndex = i;
+        //     }
+        //
+        //     if (newPanelIndex != currentPanelIndex)
+        //     {
+        //         StopCoroutine("DisablePreviousPanel");
+        //
+        //         currentPanel = panels[currentPanelIndex].panelObject;
+        //         currentPanelIndex = newPanelIndex;
+        //         nextPanel = panels[currentPanelIndex].panelObject;
+        //         nextPanel.SetActive(true);
+        //
+        //         currentPanelAnimator = currentPanel.GetComponent<Animator>();
+        //         nextPanelAnimator = nextPanel.GetComponent<Animator>();
+        //
+        //         currentPanelAnimator.SetFloat("Anim Speed", animationSpeed);
+        //         currentPanelAnimator.CrossFade(panelFadeOut, animationSmoothness);
+        //         nextPanelAnimator.SetFloat("Anim Speed", animationSpeed);
+        //         nextPanelAnimator.CrossFade(panelFadeIn, animationSmoothness);
+        //
+        //         StartCoroutine("DisablePreviousPanel");
+        //
+        //         if (panels[currentButtonIndex].panelButton != null)
+        //             currentButton = panels[currentButtonIndex].panelButton;
+        //
+        //         currentButtonIndex = newPanelIndex;
+        //
+        //         if (panels[currentButtonIndex].panelButton != null)
+        //         {
+        //             nextButton = panels[currentButtonIndex].panelButton;
+        //             currentButtonAnimator = currentButton.GetComponent<Animator>();
+        //             nextButtonAnimator = nextButton.GetComponent<Animator>();
+        //             currentButtonAnimator.Play(buttonFadeOut);
+        //             nextButtonAnimator.Play(buttonFadeIn);
+        //             
+        //         }
+        //
+        //         if (panels[currentPanelIndex].defaultSelected != null && gamepadEnabled == true)
+        //             EventSystem.current.SetSelectedGameObject(panels[currentPanelIndex].defaultSelected);
+        //     }
+        // }
+        
+        
+
+        
+    }
+}
